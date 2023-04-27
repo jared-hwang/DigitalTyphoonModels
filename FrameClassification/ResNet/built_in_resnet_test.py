d7709be717290cf3c4b3af1b9ec17bea296d9b02
jared-hwang <jared.hwang@gmail.com>
2023-04-27 16:24:38 +0900

    Add in train and testloader instead of manual batching

--- FrameClassification/ResNet/built_in_resnet_test.py
+++ FrameClassification/ResNet/built_in_resnet_test.py
@@ -6,7 +6,7 @@ import torch.nn as nn
 from pathlib import Path
 
 from train_utils import train, validate
-from model import ResidualBlock, ResNet
+from torch.utils.data import DataLoader
 from DigitalTyphoonDataloader.DigitalTyphoonDataset import DigitalTyphoonDataset
 
 import argparse
@@ -15,6 +15,7 @@ parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('--dataroot', required=True, type=str, help='path to the root data directory')
 parser.add_argument('--savepath', required=True, type=str, help='path to the directory to save the models and logs')
 parser.add_argument('--loaddata', default=False, type=bool, help='path to the root data directory')
+parser.add_argument('--small', default=False, type=bool, help='Bool to use small or full dataset')
 args = parser.parse_args()
 dataroot = Path(args.dataroot)
 data_path = dataroot
@@ -23,6 +24,8 @@ if args.loaddata:
     load_data = 'all_data'
 else:
     load_data = False
+
+use_small = args.small
 
 # data_path = Path.home() / 'data'
 images_path = str(data_path / 'image') + '/'
@@ -35,7 +38,7 @@ torch.manual_seed(SEED)
 
 # Create the model
 num_classes = 7
-num_epochs = 30
+num_epochs = 1
 batch_size = 16
 learning_rate = 0.01
 
@@ -56,26 +59,24 @@ optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=
 dataset = DigitalTyphoonDataset(str(images_path),
                                 str(track_path),
                                 str(metadata_path),
+                                'grade',
                                 load_data_into_memory=load_data,
                                 verbose=False)
 
+if use_small:
+    train_set, test_set, _ = dataset.random_split([0.01, 0.01, 0.98], split_by='frame')
+else:
+    train_set, test_set = dataset.random_split([0.8, 0.2], split_by='frame')
 
-# Open the dataset
-# dataset = DigitalTyphoonDataset("/Users/jaredhwang/PycharmProjects/typhoonDataset/tests/test_data_files/image/",
-#                                 "/Users/jaredhwang/PycharmProjects/typhoonDataset/tests/test_data_files/track/",
-#                                 "/Users/jaredhwang/PycharmProjects/typhoonDataset/tests/test_data_files/metadata.json",
-#                                 verbose=False)
-
-train_set, test_set = dataset.random_split([0.8, 0.2], split_by='frame')
-#train_set, test_set, _ = dataset.random_split([0.05, 0.05, 0.9])
-train_indices = train_set.indices
-
-train_log_string = train(model, dataset, train_set, optimizer, criterion, num_epochs, batch_size, device, save_path)
-val_loss, val_acc, val_f1_result = validate(model, dataset, test_set, criterion, device, save_path)
-train_log_string += f"Validation: \n \t loss: {val_loss} \n \t acc: {val_acc} \n \t val_f1: {val_f1_result}"
+trainloader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
+train_log_string = train(model, trainloader, optimizer, criterion, num_epochs, device, save_path)
 
 curr_time_str = str(datetime.datetime.now().strftime("%Y_%m_%d-%H.%M.%S"))
-torch.save(model.state_dict(), str(save_path / 'saved_models' / f'built_in_resnet_weights_{curr_time_str}'))
+testloader = DataLoader(test_set, batch_size=batch_size, shuffle=False)
+val_log_string = validate(model, testloader, criterion, device, curr_time_str, save_path)
+train_log_string += val_log_string
+
+torch.save(model.state_dict(), str(save_path / 'saved_models' / f'built_in_resnet_weights_{curr_time_str}.pt'))
 
 with open(str(save_path / 'logs' / f'built_in_resnet_log_{curr_time_str}'), 'w') as writer:
     writer.write(train_log_string)
--- FrameClassification/ResNet/train_utils.py
+++ FrameClassification/ResNet/train_utils.py
@@ -12,200 +12,120 @@ from sklearn.metrics import f1_score, confusion_matrix, accuracy_score
 from torchvision import datasets, transforms, models
 
 
-def train(model, dataset, train_indices, optimizer, criterion,
-          epochs, batch_size, device, savepath):
-    batches_per_epoch = len(train_indices) // batch_size
+def train(model, trainloader, optimizer, criterion,
+          epochs, device, savepath):
+    batches_per_epoch = len(trainloader)
     log_string = ''
+    model.train()
+
     for epoch in range(epochs):
-        print(f"Epoch: {epoch+1}")
+        print(f"Epoch: {epoch + 1}")
         train_running_loss = 0.0
         train_running_correct = 0
         total = 0
-       
-        model.train()
-	# for i in range(batches_per_epoch):
-        for i in tqdm(range(batches_per_epoch), total=batches_per_epoch):
-            start = i * batch_size
-            batch_indices = train_indices.indices[start:start + batch_size]
-
-            # take a batch
-            Xbatch = dataset.images_as_tensor(batch_indices)
-            Xbatch = torch.reshape(Xbatch, [batch_size, 1, 512, 512]).to(device)
-            ybatch = dataset.labels_as_tensor(batch_indices, 'grade').long().to(device)
+        for batch_num, data in enumerate(tqdm(trainloader)):
+            images, labels = data
+            images, labels = torch.Tensor(images).float(), torch.Tensor(labels).long()
+            images = torch.reshape(images, [images.size()[0], 1, images.size()[1], images.size()[2]])
+            images = images.to(device)
+            labels = labels.to(device)
 
             optimizer.zero_grad()
-            # forward pass
-            y_pred = model(Xbatch)
 
-            # Calculate the loss.
-            loss = criterion(y_pred, ybatch)
+            # Forward pass
+            predictions = model(images)
+
+            # Calculate the loss
+            loss = criterion(predictions, labels)
             train_running_loss += loss.item()
 
             # Calculate the accuracy.
-            _, predicted = torch.max(y_pred.data, 1)
-            total += ybatch.size(0)
-            train_running_correct += (predicted == ybatch).sum().item()
+            _, predicted = torch.max(predictions.data, 1)
+            total += labels.size(0)
+            train_running_correct += (predicted == labels).sum().item()
 
             # backward pass
             loss.backward()
             # update weights
             optimizer.step()
+            return 'hello'
+
 
         # Loss and accuracy for the complete epoch.
-        epoch_loss = train_running_loss / (batches_per_epoch)
-        epoch_acc = 100. * (train_running_correct / len(train_indices))
+        epoch_loss = train_running_loss / batches_per_epoch
+        epoch_acc = 100. * (train_running_correct / total)
         print(f"\t Avg batch Loss: {epoch_loss}")
         print(f"\t Accuracy: {epoch_acc}%")
-        log_string += f"Epoch {epoch+1} \n \t loss: {epoch_loss} \n \t acc: {epoch_acc} \n"
+        log_string += f"Epoch {epoch + 1} \n \t loss: {epoch_loss} \n \t acc: {epoch_acc} \n"
 
         # Checkpoint
         torch.save({
-            'epoch': epoch+1,
+            'epoch': epoch + 1,
             'model_state_dict': model.state_dict(),
             'optimizer_state_dict': optimizer.state_dict(),
-        }, str(savepath / 'saved_models' / f'model_checkpoint_epoch{epoch+1}.pt'))
+        }, str(savepath / 'saved_models' / f'model_checkpoint_epoch{epoch + 1}.pt'))
+
     return log_string
 
-# # Validation function.
-def validate(model, dataset, testloader, criterion, device, savepath):
-    model.eval()
+
+def validate(model, testloader, criterion, device, timestring, savepath):
     print('Validation')
-    predicted_labels = []
-    truth_labels = []
+    model.eval()
     valid_running_loss = 0.0
-    valid_running_correct = 0
-    counter = 0
+
+    truth_labels = []
+    predicted_labels = []
+
     with torch.no_grad():
-        for i, data in tqdm(enumerate(testloader), total=len(testloader)):
-            counter += 1
+        for batch_num, data in enumerate(tqdm(testloader)):
+            images, labels = data
+            images, labels = images.float(), labels.long()
+            images = torch.reshape(images, [images.size()[0], 1, images.size()[1], images.size()[2]]).to(device)
+            labels = labels.to(device)
 
-            image = torch.reshape(torch.Tensor(testloader[i].image()), [1, 1, 512, 512]).to(device)
-            label = torch.Tensor([testloader[i].grade()]).long().to(device)
+            # Forward pass
+            predictions = model(images)
 
-            # Forward pass.
-            outputs = model(image)
+            predicted_label = torch.argmax(predictions, 1).to('cpu')
+            predicted_labels.extend(predicted_label)
+            truth_labels.extend(labels.to('cpu'))
 
-            
-            truth_labels.append(float(label[0].to('cpu')))
-            predicted_label = torch.argmax(outputs).to('cpu')
-            predicted_labels.append(float(predicted_label))
-
-            # Calculate the loss.
-            loss = criterion(outputs, label)
+            # Calculate the loss
+            loss = criterion(predictions, labels)
             valid_running_loss += loss.item()
 
-            # Calculate the accuracy.
-            _, preds = torch.max(outputs.data, 1)
-            valid_running_correct += (preds == label).sum().item()
 
-    
-    # Loss and accuracy for the complete epoch.
-    epoch_loss = valid_running_loss
-    epoch_acc = 100. * (valid_running_correct / len(testloader))
-    f1_result = f1_score(truth_labels, predicted_labels, average='weighted')
-    accuracy = 100*accuracy_score(truth_labels, predicted_labels)
-    print(f'epoch acc: {epoch_acc}, sklearn acc: {accuracy}')
-    epoch_acc = accuracy
+        # Loss and accuracy for the complete epoch.
+        epoch_loss = valid_running_loss
+        micro_f1_result = f1_score(truth_labels, predicted_labels, average='micro')
+        macro_f1_result = f1_score(truth_labels, predicted_labels, average='macro')
+        weighted_f1_result = f1_score(truth_labels, predicted_labels, average='weighted')
+        accuracy = 100 * accuracy_score(truth_labels, predicted_labels)
 
-    num_classes = 6
-    # Build confusion matrix
-    cf_matrix = confusion_matrix(truth_labels, predicted_labels)
-    df_cm = pd.DataFrame(cf_matrix, index=[i for i in range(num_classes)],
-                         columns=[i for i in range(num_classes)])    
-     
-    curr_time_str = str(datetime.datetime.now().strftime("%d_%m_%Y-%H.%M.%S"))
-    plt.figure(figsize=(12, 7))
-    sn.heatmap(df_cm, annot=True, fmt='g')
-    plt.savefig(str(savepath / 'logs' / f'resnet_confusion_matrix_{curr_time_str}.png'))
-    
-    #normalize confusion mat
-    plt.clf()
-    df_cm = pd.DataFrame(cf_matrix / np.sum(cf_matrix, axis=1)[:, None], index=[i for i in range(num_classes)],
-                         columns=[i for i in range(num_classes)])
-    sn.heatmap(df_cm, annot=True, fmt='g')
-    plt.savefig(str(savepath / 'logs' / f'resnet_confusion_matrix_norm_{curr_time_str}.png'))
+        num_classes = 6
+        # Build confusion matrix
+        cf_matrix = confusion_matrix(truth_labels, predicted_labels)
+        df_cm = pd.DataFrame(cf_matrix, index=[i for i in range(num_classes)],
+                             columns=[i for i in range(num_classes)])
 
-    print(f"\t Loss: {epoch_loss}")
-    print(f"\t Accuracy: {epoch_acc}%")
-    print(f"\t Weighted average F1 Score: {f1_result}")
-    return epoch_loss, epoch_acc, f1_result
+        plt.figure(figsize=(12, 7))
+        sn.heatmap(df_cm, annot=True, fmt='g')
+        plt.savefig(str(savepath / 'logs' / f'resnet_confusion_matrix_{timestring}.png'))
 
+        # Save normalized confusion matrix
+        plt.clf()
+        df_cm = pd.DataFrame(cf_matrix / np.sum(cf_matrix, axis=1)[:, None], index=[i for i in range(num_classes)],
+                             columns=[i for i in range(num_classes)])
+        sn.heatmap(df_cm, annot=True, fmt='g')
+        plt.savefig(str(savepath / 'logs' / f'resnet_confusion_matrix_norm_{timestring}.png'))
 
-# def validate(model, dataset, test_indices, device):
-#     # evaluate trained model with test set
-#     with torch.no_grad():
-#         y_pred = model(X)
-#     accuracy = (y_pred.round() == y).float().mean()
-#     print("Accuracy {:.2f}".format(accuracy * 100))
+        log_string = ''
+        log_string += f"Validation: \n \t loss: {epoch_loss} \n \t acc: {accuracy} \n \t micro_f1: {micro_f1_result} " \
+                      f"\n \t macro_f1: {macro_f1_result}\n \t weighted_f1: {weighted_f1_result}"
 
-
-# def train(model, trainloader, optimizer, criterion, device):
-#     model.train()
-#     print('Training')
-#     train_running_loss = 0.0
-#     train_running_correct = 0
-#     counter = 0
-#     for i, data in tqdm(enumerate(trainloader), total=len(trainloader)):
-#         counter += 1
-#         image = torch.from_numpy(data.image())
-#         labels = torch.FloatTensor([data.grade()])
-#
-#         # image, labels = data
-#         image = image.to(device)
-#         labels = labels.to(device)
-#         optimizer.zero_grad()
-#
-#         # Forward pass.
-#         outputs = model(image)
-#         print(outputs)
-#         exit()
-#
-#         # Calculate the loss.
-#         loss = criterion(outputs, labels)
-#         train_running_loss += loss.item()
-#
-#         # Calculate the accuracy.
-#         _, preds = torch.max(outputs.data, 1)
-#         train_running_correct += (preds == labels).sum().item()
-#
-#         # Backpropagation
-#         loss.backward()
-#         # Update the weights.
-#         optimizer.step()
-#
-#     # Loss and accuracy for the complete epoch.
-#     epoch_loss = train_running_loss / counter
-#     epoch_acc = 100. * (train_running_correct / len(trainloader.dataset))
-#     return epoch_loss, epoch_acc
-#
-#
-# # Validation function.
-# def validate(model, testloader, criterion, device):
-#     model.eval()
-#     print('Validation')
-#     valid_running_loss = 0.0
-#     valid_running_correct = 0
-#     counter = 0
-#     with torch.no_grad():
-#         for i, data in tqdm(enumerate(testloader), total=len(testloader)):
-#             counter += 1
-#
-#             image, labels = data
-#             image = image.to(device)
-#             labels = labels.to(device)
-#             # Forward pass.
-#             outputs = model(image)
-#
-#             # Calculate the loss.
-#             loss = criterion(outputs, labels)
-#             valid_running_loss += loss.item()
-#
-#             # Calculate the accuracy.
-#             _, preds = torch.max(outputs.data, 1)
-#             valid_running_correct += (preds == labels).sum().item()
-#
-#     # Loss and accuracy for the complete epoch.
-#     epoch_loss = valid_running_loss / counter
-#     epoch_acc = 100. * (valid_running_correct / len(testloader.dataset))
-#     return epoch_loss, epoch_acc
-
+        print(f"\t Loss: {epoch_loss}")
+        print(f"\t Accuracy: {accuracy}%")
+        print(f"\t Micro F1 Score: {micro_f1_result}")
+        print(f"\t Macro average F1 Score: {macro_f1_result}")
+        print(f"\t Weighted average F1 Score: {weighted_f1_result}")
+        return log_string
