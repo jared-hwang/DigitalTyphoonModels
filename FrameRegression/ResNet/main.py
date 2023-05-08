90ec9f4ae4e2d8bbe12bf74029bbd00c1e8b2995
LucasSnufkin <luca.gautiers@gmail.com>
2023-05-09 03:05:31 +0900

    modifs

--- FrameRegression/ResNet/main.py
+++ FrameRegression/ResNet/main.py
@@ -1,13 +1,10 @@
 import torch
 from torch import nn
-import pandas as pd
-import datetime
 import matplotlib.pyplot as plt
-import seaborn as sn
 import numpy as np
 
-from torch.utils.data import DataLoader
 
+from torch.utils.data import DataLoader
 from DigitalTyphoonDataloader.DigitalTyphoonDataset import DigitalTyphoonDataset
 
 from model import *
@@ -15,40 +12,34 @@ from training import *
 from testing import *
 from saving import *
 
-
-
-
 #variables :
 train_size = 0.8
 test_size = 0.1
 validation_size = 0.1
-num_class = 8
-num_workers = 4
-
+num_class = 1
+num_workers=0
 if(train_size+test_size+validation_size !=1) : print(f"WARNING : SUM OF SIZES != 1 !!")
 
-split = 'sequence'
-
+split = 'frame'
+labels_to_train = 'pressure'
 batch_size = 32
 epochs = 25
-curr_time_str = str(datetime.datetime.now().strftime("%d_%m_%Y-%H.%M.%S"))  
+path_to_save = f"/app/digtyp/FrameRegression/Vgg/models/model_resnet_regression_{epochs}_{batch_size}.pth"
 
-path_to_save = f"/app/digtyp/FrameClassification/Vgg/models/model_vgg_sequence_classification_{curr_time_str}_{epochs}_{batch_size}.pth"
-
-print(f'will be saving to :{path_to_save}')
 learning_rate= 0.001
-loss_fn = nn.CrossEntropyLoss()
+loss_fn = nn.MSELoss()
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-
+print("Warning :  This model has exploding gradient problem")
+print(f"Saving to : "+path_to_save)
 #loading data base
 dataset_obj = DigitalTyphoonDataset("/app/datasets/wnp/image/", 
                                     "/app/datasets/wnp/track/", 
                                     "/app/datasets/wnp/metadata.json", 
                                     split_dataset_by='sequence',
                                     load_data_into_memory=False,
-                                    labels='grade',
+                                    labels=labels_to_train,
                                     ignore_list=[],
                                     verbose=False)
 
@@ -56,47 +47,37 @@ dataset_obj = DigitalTyphoonDataset("/app/datasets/wnp/image/",
 train_data, test_data , validation_data = dataset_obj.random_split([train_size,test_size,validation_size],split_by=split)
 print("data size : train = ",len(train_data),", test = ",len(test_data),", validation = ",len(validation_data))
 
+
 trainloader = DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=num_workers)
 testloader = DataLoader(test_data, batch_size=batch_size, shuffle=True, num_workers=num_workers)
 validationloader = DataLoader(validation_data, batch_size=batch_size, shuffle=False, num_workers=num_workers)
 
 #creating model :
-model = model_vgg(device,num_class)
+
+model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', pretrained=False)
+model.conv1 = nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+model.fc = nn.Linear(in_features=512, out_features=1, bias=True)
+model.to(device)
 
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
 #training model
-training(device,model,loss_fn,optimizer,trainloader,testloader,batch_size,epochs)
+best_mse, best_weight, history = training(device,model,loss_fn,optimizer,trainloader,testloader,batch_size,epochs)
+
+#loading best weights
+print("Loading best weights !")
+model.load_state_dict(best_weight)
+
 
 #testing model
-accuracy,cm,f1 = testing(device,model,loss_fn,testloader,batch_size)
+mse = testing(device,model,loss_fn,validationloader,batch_size)
 
-#accuracy,cm,f1 = testing(device,model,loss_fn,validationloader,batch_size)
-
-
-
-
-
-print(cm)
-
-#plotting cm    
-df_cm = pd.DataFrame(cm, index=[i for i in range(6)],
-                        columns=[i for i in range(6)])  
-
-curr_time_str = str(datetime.datetime.now().strftime("%d_%m_%Y-%H.%M.%S"))  
-plt.figure(figsize=(12, 7))
-sn.heatmap(df_cm, annot=True, fmt='g')
-plt.savefig(f'/app/digtyp/FrameClassification/Vgg/models/vgg_seq_confusion_matrix_{curr_time_str}_{epochs}_{batch_size}.png')
-
-#normalize confusion mat
-plt.clf()
-df_cm = pd.DataFrame(cm / np.sum(cm, axis=1)[:, None], index=[i for i in range(6)],
-                        columns=[i for i in range(6)])
-
-sn.heatmap(df_cm, annot=True, fmt='g')
-plt.savefig(f'/app/digtyp/FrameClassification/Vgg/models/vgg_seq_confusion_matrix_norm_{curr_time_str}_{epochs}_{batch_size}.png')
-
+#printing results
+print("MSE: %.2f" % best_mse)
+print("RMSE: %.2f" % np.sqrt(best_mse))
+plt.plot(history)
+plt.show()
 
 #saving model
-saving(model,accuracy,epochs,batch_size,cm,f1,path_to_save)
+saving(model,epochs,batch_size,mse,history,path_to_save)
 print("Saving done to " + path_to_save)
--- FrameRegression/Vgg/main.py
+++ FrameRegression/Vgg/main.py
@@ -3,6 +3,8 @@ from torch import nn
 import matplotlib.pyplot as plt
 import numpy as np
 
+
+from torch.utils.data import DataLoader
 from DigitalTyphoonDataloader.DigitalTyphoonDataset import DigitalTyphoonDataset
 
 from model import *
@@ -15,13 +17,14 @@ train_size = 0.8
 test_size = 0.1
 validation_size = 0.1
 num_class = 1
+num_workers=0
 if(train_size+test_size+validation_size !=1) : print(f"WARNING : SUM OF SIZES != 1 !!")
 
 split = 'frame'
 labels_to_train = 'pressure'
-batch_size = 16
-epochs = 5
-path_to_save = f"./digtyp/models/model_vgg_regression_{epochs}_{batch_size}"
+batch_size = 32
+epochs = 25
+path_to_save = f"/app/digtyp/FrameRegression/Vgg/models/model_vgg_regression_{epochs}_{batch_size}.pth"
 
 learning_rate= 0.001
 loss_fn = nn.MSELoss()
@@ -29,7 +32,7 @@ loss_fn = nn.MSELoss()
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 print("Warning :  This model has exploding gradient problem")
-
+print(f"Saving to : "+path_to_save)
 #loading data base
 dataset_obj = DigitalTyphoonDataset("/app/datasets/wnp/image/", 
                                     "/app/datasets/wnp/track/", 
@@ -44,12 +47,23 @@ dataset_obj = DigitalTyphoonDataset("/app/datasets/wnp/image/",
 train_data, test_data , validation_data = dataset_obj.random_split([train_size,test_size,validation_size],split_by=split)
 print("data size : train = ",len(train_data),", test = ",len(test_data),", validation = ",len(validation_data))
 
+
+trainloader = DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+testloader = DataLoader(test_data, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+validationloader = DataLoader(validation_data, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+
 #creating model :
+
 model = model_vgg(device,num_class)
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+#model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', pretrained=False)
+#model.conv1 = nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+#model.fc = nn.Linear(in_features=512, out_features=1, bias=True)
+model.to(device)
+
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
 #training model
-best_mse, best_weight, history = training(device,model,loss_fn,optimizer,train_data,test_data,batch_size,epochs)
+best_mse, best_weight, history = training(device,model,loss_fn,optimizer,trainloader,testloader,batch_size,epochs)
 
 #loading best weights
 print("Loading best weights !")
@@ -57,7 +71,7 @@ model.load_state_dict(best_weight)
 
 
 #testing model
-mse = testing(device,model,loss_fn,validation_data,batch_size)
+mse = testing(device,model,loss_fn,validationloader,batch_size)
 
 #printing results
 print("MSE: %.2f" % best_mse)
